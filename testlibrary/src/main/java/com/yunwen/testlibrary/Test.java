b2041ad2a05307038ecd80d8c912a442b8079078
xj <jxu@iyunwen.com>
2018-09-10 15:28:10 +0800

    Initial commit

---
+++ testlibrary/src/main/java/com/yunwen/testlibrary/Test.java
@@ -0,0 +1,14 @@
+package com.yunwen.testlibrary;
+
+import android.util.Log;
+
+/**
+ * Created by yunwen on 2018/9/10.
+ */
+
+public class Test {
+
+    private void test() {
+        Log.e("","fff");
+    }
+}
